abb5e546c00c3f17edb5df08f961ed31e260fb41
Anshul Gupta <1anshulgup@gmail.com>
2023-03-21 16:13:07 -0700

    Adding Navigation

--- ride-along/post-ride.js
+++ ride-along/post-ride.js
@@ -26,6 +26,53 @@ const plusBtn1 = document.getElementById("plus_btn1");
 const minusBtn1 = document.querySelector("#minus_btn1");
 const luggageNum = document.getElementById("luggage-number");
 
+const menuButton = document.getElementById('menu-button');
+const menu = document.getElementById('menu');
+
+menuButton.addEventListener('click', function () {
+    console.log("button clicked");
+    // menu.classList.remove('nonvisible');
+    menu.classList.toggle('visible');
+});
+
+const backButton1 = document.getElementById("back-button1");
+
+backButton1.addEventListener("click", function () {
+    // backButton1.classList.add("visually-hidden");
+    history.go(-1);
+});
+
+
+const myProfile = document.getElementById('profile-page');
+
+myProfile.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.location.href = `./my-profile.html?doc-id=${document_id}`;
+})
+
+const safety = document.getElementById('safety-page');
+
+safety.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.location.href = `./security/index.html?doc-id=${document_id}`;
+})
+
+const contact = document.getElementById('contact-page');
+
+contact.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.location.href = `./contact us/index.html?doc-id=${document_id}`;
+})
+
+const requestPage = document.getElementById('request-page');
+
+requestPage.addEventListener('click',(e)=>{
+    e.preventDefault();
+    window.location.href = `./request.html?doc-id=${document_id}`;
+})
+
+
+
 plusBtn1.addEventListener("click", (e) => {
     e.preventDefault();
     luggageNum.value = parseInt(luggageNum.value) + 1;
